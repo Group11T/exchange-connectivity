c9452a599b1dceeff7ec5c597720105559361503
kbbansi <kwabenaampofo5@gmail.com>
2021-03-22 17:01:22 +0000

    patch for redis setup 2

--- src/main/java/com/io/t11/exchangeconnectivity/service/OrderQueueSubscriber.java
+++ src/main/java/com/io/t11/exchangeconnectivity/service/OrderQueueSubscriber.java
@@ -4,15 +4,20 @@ import com.io.t11.exchangeconnectivity.dto.CreatedOrder;
 import com.io.t11.exchangeconnectivity.dto.OrderDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.JedisPool;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class OrderQueueSubscriber implements CommandLineRunner {
 
-    private static JedisPool jedisPool = new JedisPool("localhost");
+    @Autowired
+    private Environment env;
+
+    private  JedisPool jedisPool = new JedisPool("redis-24456-0.cloudclusters.net", 24456);
     private static final String QUEUE = "orderQueue";
     private static final int TIMEOUT = 0;
 
@@ -29,15 +34,21 @@ public class OrderQueueSubscriber implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        List<String> messages = null;
-        while(true){
-            System.out.println("Waiting for a message in the queue");
-            messages  = jedisPool.getResource().blpop(TIMEOUT,QUEUE);
-            System.out.println(messages.get(1));
-            OrderDto orderDto = convertToOrderDto(messages);
+        try {
+            List<String> messages = null;
+            while (true) {
+                System.out.println("Waiting for a message in the queue");
+                messages = jedisPool.getResource().blpop(TIMEOUT, QUEUE);
+                System.out.println(messages.get(1));
+                OrderDto orderDto = convertToOrderDto(messages);
 //            CreatedOrder order = exchangeService.callMallon(orderDto);
-            //update order uid here
+                //update order uid here
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(Arrays.toString(e.getStackTrace()));
         }
+
     }
 
     private OrderDto convertToOrderDto(List<String> messages) {
